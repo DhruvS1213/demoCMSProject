cd191a70a5c3019677be991642e06aa5ccda1f9c
unknown <1082378@01HW507711.India.TCS.com>
2016-10-14 18:13:44 +0530

    added carousel for images

--- user/umain.js
+++ user/umain.js
@@ -1,8 +1,11 @@
-angular.module('fileDownload', ['ngSanitize'])
+angular.module('fileDownload', ['ngSanitize', 'ui.bootstrap'])
+
 .controller('uvCtrl', ['$sce','$http', function($sce, $http){ 
     var vm = this;
     var temp = Math.floor((Math.random() * 100) + 1);
+    var tempimg1, tempimg2, tempimg3;
     console.log('user-view controller');
+    vm.myInterval = 3000;
     
     $http({
         method: 'GET',
@@ -37,7 +40,7 @@ angular.module('fileDownload', ['ngSanitize'])
         url: "http://localhost:3000/uploads/img.jpg?imgNo="+encodeURIComponent(1)    
     }).then(function successCallback(resp)
         {
-            vm.imageRequest1 = "http://localhost:3000/uploads/img-1.jpg";
+           vm.imageRequest1 = "http://localhost:3000/uploads/img-1.jpg";
             console.log('Image Retreival Success !!');
         }, function errorCallback(response){
             console.log(response.status);
@@ -68,11 +71,23 @@ angular.module('fileDownload', ['ngSanitize'])
             
         });
 
+        vm.slides = [
+            {
+                image: "http://localhost:3000/uploads/img-1.jpg"
+            },
+            {
+                image: "http://localhost:3000/uploads/img-2.jpg"
+            },
+            {
+                image: "http://localhost:3000/uploads/img-3.jpg"
+            },
+            {
+                image: "http://localhost:3000/uploads/img-1.jpg"
+            }
+        ];
+
+        
+
    vm.videoSRC = "http://localhost:3000/uploads/vid.mp4?t="+ temp;
-}])
+}]);
     
-
-
-
-
-
